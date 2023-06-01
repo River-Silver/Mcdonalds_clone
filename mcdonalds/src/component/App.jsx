dfd547a5900286f9a5e426de43ac594bfd499639
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-06-01 10:44:06 +0900

    REFACTOR :: 스타일드컴포넌트 적용

--- mcdonalds/src/component/App.jsx
+++ mcdonalds/src/component/App.jsx
@@ -1,23 +1,14 @@
 import Main from "../components/common/Main/mainIng";
 import Card from "../components/common/card/card";
 import Layout from "../Layout/Layout";
-// import styled from "styled-components";
 
 const App = () => {
   return (
-    <div>
-      <Layout>
-        <Main />
-        <Card />
-      </Layout>
-    </div>
+    <Layout>
+      <Main />
+      <Card />
+    </Layout>
   );
 };
 
 export default App;
-
-// const StyledEntire = styled.div`
-//   width: 1920px;
-//   display: flex;
-//   flex-direction: column;
-// `;스타일드컴포넌트왜안돼
--- mcdonalds/src/components/common/card/card.jsx
+++ mcdonalds/src/components/common/card/card.jsx
@@ -2,6 +2,7 @@ import "./card.css";
 import MacGridle from "./img/MacGridle.jpg";
 import Blt from "./img/Blt.jpg";
 import Wing from "./img/Wing.jpg";
+import styled from "styled-components";
 
 const Card = () => {
   function Cards(props) {
@@ -15,56 +16,86 @@ const Card = () => {
 
   return (
     <div>
-      <div className="titMain">McDonalds's LIVE </div>
-      <div className="cardList">
-        <div className="cardImg">
+      <TitMain>McDonalds's LIVE </TitMain>
+      <CardList>
+        <CardImg>
           <Cards
             img={Blt}
             title="5월엔 맥을 더 크고 풍성하게! 더블 빅맥 & 빅맥 BLT 출시"
             className="cardImg"
           />
-        </div>
-        <div className="cardImg">
+        </CardImg>
+        <CardImg>
           <Cards
             img={Wing}
             title="돌아온 맥윙의 맛! 겉바속쫄 맥윙!"
             className="cardImg"
           />
-        </div>
-        <div className="cardImg">
+        </CardImg>
+        <CardImg>
           <Cards
             img={MacGridle}
             title="단짠속촉 맥그리들 is back!"
             className="cardImg"
           />
-        </div>
-      </div>
+        </CardImg>
+      </CardList>
 
-      <div className="cardList">
-        <div className="cardImg">
+      <CardList>
+        <CardImg>
           <Cards
             img={Blt}
             title="5월엔 맥을 더 크고 풍성하게! 더블 빅맥 & 빅맥 BLT 출시"
             className="cardImg"
           />
-        </div>
-        <div className="cardImg">
+        </CardImg>
+        <CardImg>
           <Cards
             img={Wing}
             title="돌아온 맥윙의 맛! 겉바속쫄 맥윙!"
             className="cardImg"
           />
-        </div>
-        <div className="cardImg">
+        </CardImg>
+        <CardImg>
           <Cards
             img={MacGridle}
             title="단짠속촉 맥그리들 is back!"
             className="cardImg"
           />
-        </div>
-      </div>
+        </CardImg>
+      </CardList>
     </div>
   );
 };
 
 export default Card;
+
+const TitMain = styled.div`
+  margin: 0 0 60px;
+  color: #292929;
+  font-size: 40px;
+  font-weight: bold;
+  line-height: 1;
+`;
+
+const CardList = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  width: 1203px;
+  margin-left: 100px;
+  margin-bottom: 40px;
+`;
+
+const CardImg = styled.div`
+  width: 366px;
+  height: 420px;
+  color: #292929;
+  font-size: 20px;
+  padding: 20px;
+  line-height: 30px;
+  font-weight: bold;
+  box-shadow: 0 8px 8px -4px gray;
+  border: 1px solid black;
+  border-radius: 20px 20px 0px 0px;
+`;
