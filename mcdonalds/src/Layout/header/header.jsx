129b549294ae8d9577c1b97f71bea09b8c75997e
SeunghoonSeunghoon <nicebrian0328@gmail.com>
2023-06-07 08:42:46 +0900

    ADD :: header & footer complete

--- mcdonalds/src/Layout/header/header.jsx
+++ mcdonalds/src/Layout/header/header.jsx
@@ -10,24 +10,54 @@ const Header = () => {
         <div>
           <img src={logo} className="img"></img>
         </div>
-
+        
         <nav>
-          <ul>
-            <li className="li">
+          <ul >
+            <li>
               <h4>Menu</h4>
+              <div class="dropdown-content">
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>버거</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>맥런치</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>맥모닝</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>해피 스낵</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>사이드 & 디저트</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>맥카페 & 음료</a>
+              <a href = "#" className="dropli" style={{fontSize:"16px",color:"gray"}}>해피밀</a>
+              <div class="tt">
+              <a href = "#" className="dropli2" style={{fontSize:"16px",color:"gray"}}>매장찾기</a>
+              <a href = "#" className="dropli2" style={{fontSize:"16px",color:"gray"}}>맥딜리버리</a>
+              <a href = "#" className="dropli2" style={{fontSize:"16px",color:"gray"}}>맥드라이브</a>
+              <a href = "#" className="dropli2" style={{fontSize:"16px",color:"gray"}}>임차문의</a>
+              </div>
+              <div class = "tt2">
+              <a href = "#" className = "dropli3"style={{fontSize:"16px",color:"gray"}}>프로모션</a>
+              <a href = "#" className = "dropli3"style={{fontSize:"16px",color:"gray"}}>새로운 소식</a>
+              <a href = "#" className = "dropli3"style={{fontSize:"16px",color:"gray"}}>이달의 해피밀</a>
+              </div>
+              <div class = "tt3">
+              <a href = "#" className = "dropli4"style={{fontSize:"16px",color:"gray"}}>브랜드 소개</a>
+              <a href = "#" className = "dropli4"style={{fontSize:"16px",color:"gray"}}>사회적 책임과 지원</a>
+              <a href = "#" className = "dropli4"style={{fontSize:"16px",color:"gray"}}>맥도날드 품질 이야기</a>
+              <a href = "#" className = "dropli4"style={{fontSize:"16px",color:"gray"}}>맥도날드 사람들</a>
+
+              </div>
+               
+              </div>
             </li>
-            <li className="li">
+            <li>
               <h4>Store</h4>
             </li>
 
-            <li className="li">
+            <li>
               <h4>What's New</h4>
             </li>
 
-            <li className="li">
+            <li>
               <h4>Story</h4>
             </li>
+            
           </ul>
+
           <li className="li2">
             <button className="imcha">
               <h4>임차문의</h4>
