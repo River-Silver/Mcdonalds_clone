012429f90aa3b60cb6f1c37857e40229d5a646d6
변은혜 <102288426+byuneunhye@users.noreply.github.com>
2023-06-15 10:04:56 +0900

    test

--- mcdonalds/src/component/App.jsx
+++ mcdonalds/src/component/App.jsx
@@ -1,4 +1,3 @@
-// import Main from "../components/common/Main/mainIng";
 import Card from "../components/common/card/card";
 import Layout from "../Layout/Layout";
 import Slider from "../components/common/Main/swipe";
@@ -6,7 +5,6 @@ import Slider from "../components/common/Main/swipe";
 const App = () => {
   return (
     <Layout>
-      {/* <Main /> */}
       <Slider />
       <Card />
     </Layout>
