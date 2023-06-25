07cb70f7aac960cfbd04757a9290adaa8eedf80a
SeunghoonSeunghoon <nicebrian0328@gmail.com>
2023-06-25 23:58:13 +0900

    UPDATE :: 카카오맵 삽입 도전중

--- mcdonalds/src/component/search/search.jsx
+++ mcdonalds/src/component/search/search.jsx
@@ -8,6 +8,16 @@ import Delivery from "./assets/delivery.png";
 import Drive from "./assets/drive.png";
 import Mcmorning from "./assets/mcmorning.png";
 import Parking from "./assets/parking.png";
+import * as K from "react-kakao-maps-sdk";
+
+const test = () => {
+	const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=5b80c7e1b82d03956b88c1f5d193a0f6&libraries=services,clusterer";
+    document.head.appendChild(script);
+}
+
+test();
 const SearchMc = () => {
   return (
     <Layout>
@@ -40,11 +50,22 @@ const SearchMc = () => {
               </S.Buttoncomp>
             </S.CompDiv>
             <S.SearchDiv>
-              <S.SearchWord placeholder={"매장명, 동명, 도로명을 검색해주세요."}></S.SearchWord>
+              <S.SearchWord
+                placeholder={"매장명, 동명, 도로명을 검색해주세요."}
+              ></S.SearchWord>
               <S.SearchButton>검색하기</S.SearchButton>
             </S.SearchDiv>
           </S.Mid>
         </S.Nmain>
+        <K.Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: "1000px", height: "360px" }}
+      
+    >
+      <K.MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{color:"#000"}}>Hello World!</div>
+      </K.MapMarker>
+    </K.Map>
       </S.DeMain>
     </Layout>
   );
